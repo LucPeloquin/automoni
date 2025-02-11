16262148dee715b898cf30de1fe26917f4d2ee99
Luc <103437585+LucPeloquin@users.noreply.github.com>
2025-02-10 18:52:05 -0800

    Update main.py headless compatibility
    Made headless work on grailed

--- main.py
+++ main.py
@@ -35,10 +35,16 @@ def send_push_notification(title, message):
 
 def fetch_listing_count(url):
   chrome_options = Options()
-  # chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--headless=new')
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--disable-dev-shm-usage')
   chrome_options.add_argument("start-maximized")
+  chrome_options.add_argument("--window-size=1920,1080")
+  chrome_options.add_argument("--disable-gpu")
+  chrome_options.add_argument("--remote-debugging-port=9222")
+  chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+)
   # chrome_options.add_argument("--user-data-dir=selenium") #Saves browser history
   # chrome_options.add_experimental_option("prefs",{"download.default_directory" : "/home/runner/{REPL_NAME}"}) #Download Directory
 
